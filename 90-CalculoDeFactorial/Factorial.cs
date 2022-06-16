994a295ce081989a1b206f8b42c10d5a07748c07
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:57:34 -0300

    Add files via upload

---
+++ 90-CalculoDeFactorial/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _90_CalculoDeFactorial
+{
+    class Factorial
+    {
+        
+        public int operacion (int num)
+        {
+            int resultado = num;
+            for (int i = num - 1; i > 1; i--) //empiezo en num-1 y acabo en 1 (no llega a 1)
+            {
+                resultado *= i;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ 90-CalculoDeFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Crea una función que calcule el factorial de un número.
+
+namespace _90_CalculoDeFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Factorial factor = new Factorial();
+
+            int resultado = factor.operacion(5);
+
+            Console.WriteLine("El facotorial del numero ingresado es: " + resultado);
+        }
+    }
+}
